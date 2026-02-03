217069dd7ee947f774033298071dcb0ec65f247d
Vinh-Nguyen-saltovn <vinh.nguyenpt@salto.vn>
2026-02-03 16:21:26 +0700

    test pre-commit

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const x = 1
 const mainButton = document.querySelector('.main-button')
 mainButton.addEventListener('click', () => {
   x = 3
-  alert(`Xin chào lần ${x}`)
+  alert(`Xin chào lần thứ ${x}`)
 })
