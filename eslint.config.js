bb0f5c3dedbb206f170920ecdd9c0463a0da9efd
Vinh-Nguyen-saltovn <vinh.nguyenpt@salto.vn>
2026-02-25 17:37:27 +0700

    remove unused nextjs files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 export default [
-  { ignores: ['**/.next/**', 'nextjs/**'] },
+  { ignores: ['**/.next/**', 'nextjs-project/**'] },
   {
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
