34e7bfdca41b8991588fe6bb0b777262d6f867a3
Vinh-Nguyen-saltovn <vinh.nguyenpt@salto.vn>
2026-02-25 10:37:46 +0700

    check branch

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const x = 1
+let x = 1
 
 const mainButton = document.querySelector('.main-button')
 mainButton.addEventListener('click', () => {
+  x = 4
   alert(`xin chào bạn lần thứ  : ${x}`)
 })
