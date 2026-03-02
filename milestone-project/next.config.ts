c737837a94cfdf5323712dea5bc425e730767b3b
Vinh-Nguyen-saltovn <vinh.nguyenpt@salto.vn>
2026-03-02 14:33:15 +0700

    set-up source cho dự án mới

---
+++ milestone-project/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: {
+    compilationMode: 'infer',
+  },
+}
+
+export default nextConfig
